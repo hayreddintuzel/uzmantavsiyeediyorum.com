f4bf670bbfec36a7da6467009dc84077494e4130
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2019-01-31 20:05:48 +0300

    deleted docdirect

---
+++ wp-content/docdirect-child_old/framework-customizations/extensions/shortcodes/shortcodes/top-categories_ERROR/views/view.php
@@ -0,0 +1,108 @@
+<?php
+if (!defined('FW')) {
+    die('Forbidden');
+}
+/**
+ * @var $atts
+ */
+global $wpdb;
+$uni_flag = fw_unique_increment();
+$dir_search_page = fw_get_db_settings_option('dir_search_page');
+if( isset( $dir_search_page[0] ) && !empty( $dir_search_page[0] ) ) {
+	$search_page 	 = get_permalink((int)$dir_search_page[0]);
+} else{
+	$search_page 	 = '';
+}
+
+$args = array('posts_per_page' => '-1', 
+			   'post_type' => 'directory_type', 
+			   'post_status' => 'publish',
+			   'suppress_filters' => false
+		);
+
+if( !empty( $atts['categories'] ) ){
+	$args['post__in']	= $atts['categories'];
+}
+$cust_query = get_posts($args);
+?>
+
+<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-offset-0 col-sm-12 col-xs-12">
+    <div class="doc-section-head">
+        <?php if( !empty( $atts['heading'] ) && !empty( $atts['sub_heading'] ) ){?>
+            <div class="doc-section-heading">
+                <?php if( !empty( $atts['heading'] ) ){?>
+                    <h2><?php echo esc_attr( $atts['heading'] );?></h2>
+                <?php }?>
+                <?php if( !empty( $atts['sub_heading'] ) ){?>
+                    <span><?php echo esc_attr( $atts['sub_heading'] );?></span>
+                <?php }?>
+            </div>
+        <?php }?>
+        <?php if( !empty( $atts['description'] ) ){?>
+        <div class="doc-description">
+            <?php echo do_shortcode( $atts['description'] );?>
+        </div>
+        <?php }?>
+    </div>
+</div>
+<div class="doc-topcategories">
+    <?php 
+	if( isset( $cust_query ) && !empty( $cust_query ) ) {
+	  $counter	= 0;
+	  
+	  foreach ($cust_query as $key => $dir) {
+			$counter++;
+			$title = get_the_title($dir->ID);
+			$category_image = fw_get_db_post_option($dir->ID, 'category_image', true);
+
+			if( !empty( $category_image['attachment_id'] ) ){
+				$banner	= docdirect_get_image_source($category_image['attachment_id'],470,305);
+	  		} else{
+		 		$banner	= get_template_directory_uri().'/images/user470x305.jpg';;
+		 	}
+			
+			/*$user_query = "SELECT DISTINCT COUNT(*) as total
+							FROM ".$wpdb->prefix."users 
+							INNER JOIN ".$wpdb->prefix."usermeta ON ( ".$wpdb->prefix."users.ID = ".$wpdb->prefix."usermeta.user_id )  
+							INNER JOIN ".$wpdb->prefix."usermeta AS mt1 ON ( ".$wpdb->prefix."users.ID = mt1.user_id )  
+							INNER JOIN ".$wpdb->prefix."usermeta AS mt2 ON ( ".$wpdb->prefix."users.ID = mt2.user_id )
+							WHERE 1=1 
+							AND ( 
+							  ( 
+								( 
+								  ( ".$wpdb->prefix."usermeta.meta_key = 'directory_type' AND ".$wpdb->prefix."usermeta.meta_value = '".$dir->ID."' ) 
+								  AND 
+								  ( mt1.meta_key = 'verify_user' AND mt1.meta_value = 'on' )
+								) 
+								AND 
+								( 
+								  ( 
+									( mt2.meta_key = '".$wpdb->prefix."capabilities' AND mt2.meta_value LIKE '%professional%' )
+								  )
+								)
+							  )
+							)";
+		  	
+		  	$total_users	= $wpdb->get_var("$user_query");*/
+			?>
+			<div class="col-md-4 col-sm-4 col-xs-6">
+				<div class="doc-category">
+					<figure class="doc-categoryimg">
+						<h4><?php echo esc_attr( $title );?></h4>
+						<p><a href="<?php echo esc_attr( $dir->post_name );?>"><img src="<?php echo esc_url( $banner );?>" alt="<?php echo esc_attr( $title );?>"></a></p>
+						<?php /*<div class="doc-hoverbg">
+							
+						</div> */?>
+						<figcaption class="doc-imghover">
+							<div class="doc-categoryname"><h4><a href="<?php echo esc_attr( $dir->post_name );?>"><?php echo esc_attr( $title );?></a></h4></div>
+							<?php /*?><span class="doc-categorycount"><a href="javascript:;"><?php echo intval( $total_users );?><i class="fa fa-clone"></i></a></span><?php */?>
+						</figcaption>
+					</figure>
+				</div>
+			</div>
+		<?php }
+		} else{
+          $directories['status']	= 'empty'; 
+        }
+        ?>
+</div>
